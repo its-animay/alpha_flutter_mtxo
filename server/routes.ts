27a5c25599f069d464c8a797c672e2712dce9525
animaytiwari321 <42453262-animaytiwari321@users.noreply.replit.com>
2025-05-16 20:49:51 +0000

    Restored to '23e0c28a69c57b1beb47f9f07772e57c0869dbab'
    Replit-Restored-To: 23e0c28a69c57b1beb47f9f07772e57c0869dbab

--- server/routes.ts
+++ server/routes.ts
@@ -9,7 +9,6 @@ import multer from "multer";
 import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
 import fs from 'fs';
-import { createDownloadableZip, generateZipFile } from "./create-download-zip";
 
 // Authentication middleware
 function isAuthenticated(req: Request, res: Response, next: NextFunction) {
